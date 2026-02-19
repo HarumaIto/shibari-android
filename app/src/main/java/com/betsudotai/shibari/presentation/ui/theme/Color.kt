989820cc8a4024f2e245258f26c09c7419d468c2
HarumaIto <itoharm@gmail.com>
2026-02-19 10:39:33 +0900

    change: presentation/uiに移動

--- app/src/main/java/com/betsudotai/shibari/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/betsudotai/shibari/presentation/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.betsudotai.shibari.ui.theme
+package com.betsudotai.shibari.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/betsudotai/shibari/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/betsudotai/shibari/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.betsudotai.shibari.ui.theme
+package com.betsudotai.shibari.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
