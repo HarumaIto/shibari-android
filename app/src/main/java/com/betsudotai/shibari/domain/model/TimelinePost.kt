a2a995b1369773755152d73de03d0962140f421e
HarumaIto <itoharm@gmail.com>
2026-02-19 10:15:53 +0900

    change: LocalDateを使用する

--- app/src/main/java/com/betsudotai/shibari/domain/model/TimelinePost.kt
+++ app/src/main/java/com/betsudotai/shibari/domain/model/TimelinePost.kt
@@ -5,6 +5,7 @@ import com.betsudotai.shibari.domain.model.timeline.QuestSnapshot
 import com.betsudotai.shibari.domain.value.MediaType
 import com.betsudotai.shibari.domain.value.PostStatus
 import com.betsudotai.shibari.domain.value.VoteType
+import java.time.LocalDate
 
 data class TimelinePost(
     val id: String,
@@ -18,5 +19,5 @@ data class TimelinePost(
     val status: PostStatus,
     val approvalCount: Int,
     val votes: Map<String, VoteType>, // user_uid to vote type
-    val createdAt: Long
+    val createdAt: LocalDate?
 )
