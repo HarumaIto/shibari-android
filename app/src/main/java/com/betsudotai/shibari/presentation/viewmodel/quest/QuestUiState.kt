2e8dd836f3765e9649ae413964fc42711acb952c
HarumaIto <itoharm@gmail.com>
2026-02-25 20:01:21 +0900

    update: グループごとにクエストを表示するように変更

--- app/src/main/java/com/betsudotai/shibari/presentation/viewmodel/quest/QuestUiState.kt
+++ app/src/main/java/com/betsudotai/shibari/presentation/viewmodel/quest/QuestUiState.kt
@@ -1,9 +1,15 @@
 package com.betsudotai.shibari.presentation.viewmodel.quest
 
 import com.betsudotai.shibari.domain.model.Quest
+import com.betsudotai.shibari.domain.value.QuestFrequency
+
+data class QuestGroup (
+    val frequency: QuestFrequency,
+    val quests: List<Quest>,
+)
 
 sealed interface QuestsUiState {
     data object Loading : QuestsUiState
-    data class Success(val myQuests: List<Quest>) : QuestsUiState
+    data class Success(val groupedQuests: List<QuestGroup>) : QuestsUiState
     data class Error(val message: String) : QuestsUiState
 }
